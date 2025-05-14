84d869b89818862c922966c3a7278b73f57a9980
big-light <xcf941743128@163.com>
2025-05-14 15:23:19 +0800

    测试

---
+++ goodmall-product/src/test/java/com/kfc/product/GoodmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfc.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodmallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
